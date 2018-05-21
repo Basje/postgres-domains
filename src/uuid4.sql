26ac9cfcc0ba6b4260557c6895dbee93911f039e
Bas Hamar de la Brethonière <b.hamar@inthere.nl>
2018-05-21 13:57:55 +0200

    Add check for naming collision of uuid4 type

--- src/uuid4.sql
+++ src/uuid4.sql
@@ -5,7 +5,7 @@
 
 -- CREATE EXTENSION "uuid-ossp";
 -- DROP DOMAIN IF EXISTS public.uuid4 CASCADE;
-CREATE DOMAIN public.uuid4 AS uuid
+CREATE DOMAIN IF NOT EXISTS public.uuid4 AS uuid
     DEFAULT uuid_generate_v4()
     CONSTRAINT uuid4_format_ck CHECK (
         VALUE ~* '^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$'
